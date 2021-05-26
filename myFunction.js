7f2e8998f29405e1dbb6f509f8f86f1117cfbd6c
Bijoya10 <63376288+Bijoya10@users.noreply.github.com>
2021-05-26 19:36:23 +0530

    Add files via upload

---
+++ myFunction.js
@@ -0,0 +1,122 @@
+
+function spawnRed() {
+    var redBlocks = createSprite(random(20, 480), random(1, 100), 30, 30);
+    redBlocks.shapeColor = "red";
+    redBlocks.velocityY = 10;
+    redBlocksGroup.add(redBlocks);
+   
+ }
+
+
+function spawnGreen() {
+    var greenBlocks = createSprite(random(20, 480), random(1, 100), 30, 30);
+    greenBlocks.shapeColor = "green";
+    greenBlocks.velocityY = 10;
+    greenBlocksGroup.add(greenBlocks);
+
+}
+
+
+function spawnPurple() {
+  var purpleBlocks = createSprite(random(20, 480), random(1, 100), 30, 30);
+  purpleBlocks.shapeColor = "purple";
+  purpleBlocks.velocityY = 10;
+  purpleBlocksGroup.add(purpleBlocks);
+  
+}
+
+
+function spawnBlue() {
+  var blueBlocks = createSprite(random(20, 480), random(1, 100), 30, 30);
+  blueBlocks.shapeColor = "blue";
+  blueBlocks.velocityY = 10;
+  blueBlocksGroup.add(blueBlocks);
+}
+
+function movement() {
+
+    if (keyDown(LEFT_ARROW)) {
+        player.velocityX = -9;
+    } else if (keyDown(RIGHT_ARROW)) {
+        player.velocityX = 9;
+    } else {
+        player.velocityX = 0;
+    }
+}
+
+function spawning(){
+    
+
+    if (frameCount % 100 === 0) {
+      rand = Math.round(random(1, 4));
+      if (rand === 4) {
+        spawnRed();
+      }
+      else if (rand === 1) {
+        spawnGreen();
+      }
+      else if (rand === 2) {
+        spawnPurple();
+      }
+      else if (rand === 3){
+        spawnBlue();
+      }
+    }
+}
+
+function scoreChanging(){
+ 
+  if(redBlocksGroup.isTouching(player)){
+    score -= 1
+    redBlocksGroup.destroyEach();
+    gameState = END;
+  }
+
+  if(purpleBlocksGroup.isTouching(player)){
+    score -= 2
+    purpleBlocksGroup.destroyEach();
+  }
+
+  if(greenBlocksGroup.isTouching(player)){
+    score += 1
+    greenBlocksGroup.destroyEach();
+  }
+  
+  if(blueBlocksGroup.isTouching(player)){
+    score += 2
+    blueBlocksGroup.destroyEach();
+  }
+  
+}
+
+function textScores(){
+    
+  textSize(10);
+  fill("white");
+  text("Score: " + score,10,20);
+  text("High Score: "+ highscore,10,30);
+}
+
+function play(){
+  movement();
+
+  spawning();
+}
+
+function reset(){
+  gameState = PLAY;
+  redBlocksGroup.destroyEach();
+  greenBlocksGroup.destroyEach();
+  player.velocityX = 0;
+  if(highScore < score){
+    highScore = score;
+  }
+}
+
+
+function adaptivity(){
+  if(score%10 === 0){
+    redBlocks.velocityY += 4;
+    greenBlocksGroup.velocityY += 4;
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,59 @@
+var player;
+var score = 0 , highscore = 0;
+var gameState, START, PLAY, END,rand;
+var redBlocksGroup, greenBlocksGroup,blueBlocksGroup, purpleBlocksGroup;
+
+function setup() {
+  createCanvas(500, 500);
+
+  START = 0;
+  PLAY = 1;
+  END = 2;
+  gameState = START;
+
+  player = createSprite(width / 2, height - 100, 40, 40);
+  player.shapeColor = "white";
+
+  redBlocksGroup = new Group();
+  greenBlocksGroup = new Group();
+  blueBlocksGroup = new Group();
+  purpleBlocksGroup = new Group();
+
+   startButton = createButton("START");
+   startButton.position(width / 2 - 25, height / 2);
+   startButton.mousePressed(()=>{
+     gameState = PLAY;
+     startButton.hide();
+    })
+   
+   resetButton = createButton("RESET");
+   resetButton.position(width / 2 - 25, 10);
+   resetButton.hide();
+ 
+  
+}
+
+
+
+function draw() {
+  background(0);
+
+  
+  if (gameState === PLAY) {
+    play();
+    scoreChanging();
+  }
+ 
+
+  if (gameState === END) {
+    resetButton.show();
+    resetButton.mousePressed(reset);
+    
+  }
+
+ // console.log(gameState);
+  drawSprites();
+
+  textScores();
+
+}
